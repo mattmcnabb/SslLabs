13f1cc215a7079bddfc30e94b2984ab6dccc6b84
Matt McNabb <mmcnabb@outlook.com>
2017-05-07 16:42:42 -0400

    fix build paths

--- deploy.ps1
+++ deploy.ps1
@@ -1,7 +1,6 @@
-﻿if ($env:APPVEYOR_REPO_COMMIT_MESSAGE -match "^!Deploy")
+if ($env:APPVEYOR_REPO_COMMIT_MESSAGE -match "^!Deploy")
 {
     $ModulePath = Join-Path $env:APPVEYOR_BUILD_FOLDER $env:APPVEYOR_PROJECT_NAME
     Import-Module PowerShellGet -Force
     Publish-Module -Path $ModulePath -NuGetApiKey ($env:PSGallery_Api_Key) -Confirm:$false
 }
-
